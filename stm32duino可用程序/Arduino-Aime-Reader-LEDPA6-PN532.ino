ff149512e2a47852ccb982e7940bced5b34ef5d0
Helloworld_Dk <32455937+HelloworldDk@users.noreply.github.com>
2022-12-05 21:30:52 +0800

    上传程序

---
+++ stm32duino可用程序/Arduino-Aime-Reader-LEDPA6-PN532.ino
@@ -0,0 +1,149 @@
+#include "cmd.h"
+bool high_baudrate = false;//high_baudrate=true
+
+void setup() {
+  nfc.begin();
+  nfc.setPassiveActivationRetries(0x10);//设定等待次数
+  nfc.SAMConfig();
+  pixels.begin();
+    pixels.setBrightness(LED_brightness);
+ while (!nfc.getFirmwareVersion()) {
+   //  FastLED.showColor(0xFF0000);
+  pixels.fill( pixels.Color(255, 0, 0));
+  pixels.show();
+  delay(500);
+  pixels.fill( pixels.Color(0, 0, 0));
+  pixels.show();
+  delay(500);
+  }
+
+  memset(&req, 0, sizeof(req.bytes));
+  memset(&res, 0, sizeof(res.bytes));
+  SerialDevice.begin(115200);
+  SerialDevice.dtr(false);
+
+
+  pixels.fill( pixels.Color(16, 16, 16));
+  pixels.show();
+}
+
+void loop() {
+  SerialCheck();
+  packet_write();
+}
+
+static uint8_t len, r, checksum;
+static bool escape = false;
+
+static uint8_t packet_read() {
+
+  while (SerialDevice.available()) {
+    r = SerialDevice.read();
+    if (r == 0xE0) {
+      req.frame_len = 0xFF;
+      continue;
+    }
+    if (req.frame_len == 0xFF) {
+      req.frame_len = r;
+      len = 0;
+      checksum = r;
+      continue;
+    }
+    if (r == 0xD0) {
+      escape = true;
+      continue;
+    }
+    if (escape) {
+      r++;
+      escape = false;
+    }
+    req.bytes[++len] = r;
+    if (len == req.frame_len && checksum == r) {
+      return req.cmd;
+    }
+    checksum += r;
+  }
+  return 0;
+}
+
+static void packet_write() {
+  uint8_t checksum = 0, len = 0;
+  if (res.cmd == 0) {
+    return;
+  }
+  SerialDevice.write(0xE0);
+  while (len <= res.frame_len) {
+    uint8_t w;
+    if (len == res.frame_len) {
+      w = checksum;
+    } else {
+      w = res.bytes[len];
+      checksum += w;
+    }
+    if (w == 0xE0 || w == 0xD0) {
+      SerialDevice.write(0xD0);
+      SerialDevice.write(--w);
+    } else {
+      SerialDevice.write(w);
+    }
+    len++;
+  }
+  res.cmd = 0;
+}
+
+void SerialCheck() {
+  switch (packet_read()) {
+    case SG_NFC_CMD_RESET:
+      sg_nfc_cmd_reset();
+      break;
+    case SG_NFC_CMD_GET_FW_VERSION:
+      sg_nfc_cmd_get_fw_version();
+      break;
+    case SG_NFC_CMD_GET_HW_VERSION:
+      sg_nfc_cmd_get_hw_version();
+      break;
+    case SG_NFC_CMD_POLL:
+      sg_nfc_cmd_poll();
+      break;
+    case SG_NFC_CMD_MIFARE_READ_BLOCK:
+      sg_nfc_cmd_mifare_read_block();
+      break;
+    case SG_NFC_CMD_FELICA_ENCAP:
+      sg_nfc_cmd_felica_encap();
+      break;
+    case SG_NFC_CMD_AIME_AUTHENTICATE:
+      sg_nfc_cmd_aime_authenticate();
+      break;
+    case SG_NFC_CMD_BANA_AUTHENTICATE:
+      sg_nfc_cmd_bana_authenticate();
+      break;
+    case SG_NFC_CMD_MIFARE_SELECT_TAG:
+      sg_nfc_cmd_mifare_select_tag();
+      break;
+    case SG_NFC_CMD_MIFARE_SET_KEY_AIME:
+      sg_nfc_cmd_mifare_set_key_aime();
+      break;
+    case SG_NFC_CMD_MIFARE_SET_KEY_BANA:
+      sg_nfc_cmd_mifare_set_key_bana();
+      break;
+    case SG_NFC_CMD_RADIO_ON:
+      sg_nfc_cmd_radio_on();
+      break;
+    case SG_NFC_CMD_RADIO_OFF:
+      sg_nfc_cmd_radio_off();
+      break;
+    case SG_RGB_CMD_RESET:
+      sg_led_cmd_reset();
+      break;
+    case SG_RGB_CMD_GET_INFO:
+      sg_led_cmd_get_info();
+      break;
+    case SG_RGB_CMD_SET_COLOR:
+      sg_led_cmd_set_color();
+      break;
+    case 0:
+      break;
+    default:
+      sg_res_init();
+  }
+}
